5fcc755b1fab1e6c378f1c6306fd311a384def88
ManideepK078 <saimanideepk.p@gmail.com>
2025-02-01 00:12:09 +0530

    Create App.js
    Just a JavaScript code

---
+++ App.js
@@ -0,0 +1,10 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import * as d3 from 'd3';
+import moment from 'moment';
+
+const App = () => {
+    // ... (rest of the code as provided in the previous response)
+};
+
+export default App;
